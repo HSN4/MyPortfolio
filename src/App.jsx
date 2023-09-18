999074a8bad3c8375a02635d5402040ddbe1f088
hussein <7ssooony14@gmail.com>
2023-09-18 21:35:44 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -8,27 +8,42 @@ import {
   Footer,
 } from "./components";
 
+import { useState, useEffect } from "react";
+
 const App = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoaded(true);
+    }, 1000);
+  }, []);
   return (
-    <div className="w-full dark:bg-dark dark:text-white ">
-      <div
-        className={`${style.paddingX} shadow-md w-full dark:bg-dark bg-white fixed z-[100] `}
-      >
-        <div className={`${style.boxWidth} `}>
-          <Navpar />
+    <>
+      {isLoaded ? (
+        <div className="w-full dark:bg-dark dark:text-white ">
+          <div
+            className={`${style.paddingX} shadow-md w-full dark:bg-dark bg-white fixed z-[100] `}
+          >
+            <div className={`${style.boxWidth} `}>
+              <Navpar />
+            </div>
+          </div>
+          <div className={`${style.paddingX} dark:bg-dark dark:text-white `}>
+            <div className={`${style.boxWidth}`}>
+              <Hero />
+            </div>
+          </div>
+          <div className={`${style.paddingX} dark:bg-dark dark:text-white`}>
+            <div className={`${style.boxWidth}`}>
+              <Skills /> <Portfoilo /> <ContactMe /> <Footer />{" "}
+            </div>
+          </div>
         </div>
-      </div>
-      <div className={`${style.paddingX} dark:bg-dark dark:text-white `}>
-        <div className={`${style.boxWidth}`}>
-          <Hero />
-        </div>
-      </div>
-      <div className={`${style.paddingX} dark:bg-dark dark:text-white`}>
-        <div className={`${style.boxWidth}`}>
-          <Skills /> <Portfoilo /> <ContactMe /> <Footer />{" "}
-        </div>
-      </div>
-    </div>
+      ) : (
+        <div className="h-[100vh] flex justify-center items-center">hi</div>
+      )}
+    </>
   );
 };
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,14 +6,6 @@ import { useEffect, useState } from "react";
 import { uiux } from "../assets";
 
 const Hero = () => {
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoaded(true);
-    }, 1000);
-  }, []);
-
   const href =
     "https://www.canva.com/design/DAFtcaNjzNw/SIS3cPfWWTJt1Q3DJxs04g/view?utm_content=DAFtcaNjzNw&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink";
   const sentens = "HUSSEIN".split("");
@@ -27,71 +19,66 @@ const Hero = () => {
 
   return (
     <>
-      {isLoaded ? (
-        <section
-          id="home"
-          className={`h-[100vh]   flex  sm:flex-row flex-col-reverse  ss:justify-between justify-center items-center `}
-        >
-          <div className="  sm:block flex relative flex-col justify-center items-center">
-            <Reveal>
-              {
-                <p className="text-[30px]   font-medium dark:text-whitecolor text-secondary ">
-                  {sentens2.map((letter, index) => (
-                    <Textspan key={index}>
-                      {letter === " " ? "\u00A0" : letter}
-                    </Textspan>
-                  ))}
-                </p>
-              }
-            </Reveal>
+      <section
+        id="home"
+        className={`h-[100vh]   flex  sm:flex-row flex-col-reverse  ss:justify-between justify-center items-center `}
+      >
+        <div className="  sm:block flex relative flex-col justify-center items-center">
+          <Reveal>
+            {
+              <p className="text-[30px]   font-medium dark:text-whitecolor text-secondary ">
+                {sentens2.map((letter, index) => (
+                  <Textspan key={index}>
+                    {letter === " " ? "\u00A0" : letter}
+                  </Textspan>
+                ))}
+              </p>
+            }
+          </Reveal>
 
-            <Reveal>
-              {
-                <h1 className="text-primary inline-block  lg:text-[90px]  text-[40px] font-extrabold">
-                  {sentens.map((letter, index) => (
-                    <Textspan key={index}>{letter}</Textspan>
-                  ))}
-                </h1>
-              }
-            </Reveal>
-            <Reveal>
-              {
-                <h2 className=" font-medium ss:text-[26px] text-[18px] mb-3 ml-2">
-                  <span className="text-primary dark:text-whitecolor">
-                    {text}
-                  </span>
-                  <Cursor className="bg-black" />
-                </h2>
-              }
-            </Reveal>
-            <Reveal>
-              {
-                <p className="max-w-[360px] sm:text-start text-center dark:text-whitecolor text-secondary font-[400] ml-2 text-[16px]">
-                  My passion lies in creating visually appealing and
-                  user-friendly interfaces that enhance the overall experience
-                  of users.
-                </p>
-              }
-            </Reveal>
+          <Reveal>
+            {
+              <h1 className="text-primary inline-block  lg:text-[90px]  text-[40px] font-extrabold">
+                {sentens.map((letter, index) => (
+                  <Textspan key={index}>{letter}</Textspan>
+                ))}
+              </h1>
+            }
+          </Reveal>
+          <Reveal>
+            {
+              <h2 className=" font-medium ss:text-[26px] text-[18px] mb-3 ml-2">
+                <span className="text-primary dark:text-whitecolor">
+                  {text}
+                </span>
+                <Cursor className="bg-black" />
+              </h2>
+            }
+          </Reveal>
+          <Reveal>
+            {
+              <p className="max-w-[360px] sm:text-start text-center dark:text-whitecolor text-secondary font-[400] ml-2 text-[16px]">
+                My passion lies in creating visually appealing and user-friendly
+                interfaces that enhance the overall experience of users.
+              </p>
+            }
+          </Reveal>
 
-            <Reveal>
-              {
-                <div className="ml-2 mt-4">
-                  <Buttons Text={"Live CV"} href={uiux} width={"w-[100px]"} />
-                </div>
-              }
-            </Reveal>
-          </div>
-          <div className="sm:h-[500px] h-[330px] mt-[30px]">
-            <iframe
-              src="https://my.spline.design/noisedisplacecopy-2617893339df9b20eb9fb2f527d0bdc1/"
-              className="md:w-[500px] w-[400px] md:h-[500px] h-[400px] ss:scale-100 scale-[0.70] "
-            ></iframe>
-          </div>
-        </section>
-      ) : (
-        <div className="h-[100vh]"></div>
-      )}
+          <Reveal>
+            {
+              <div className="ml-2 mt-4">
+                <Buttons Text={"Live CV"} href={uiux} width={"w-[100px]"} />
+              </div>
+            }
+          </Reveal>
+        </div>
+        <div className="sm:h-[500px] h-[330px] mt-[30px]">
+          <iframe
+            src="https://my.spline.design/noisedisplacecopy-2617893339df9b20eb9fb2f527d0bdc1/"
+            className="md:w-[500px] w-[400px] md:h-[500px] h-[400px] ss:scale-100 scale-[0.70] "
+          ></iframe>
+        </div>
+      </section>
     </>
   );
 };
