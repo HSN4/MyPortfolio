7c87f4b1cb9c32f3696babfd51a71fac5b18106c
hussein <7ssooony14@gmail.com>
2023-10-03 00:17:47 +0300

    edit text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="p-8 text-center  border-t-2 border-secondry font-medium border-solid">
-      © designed & developed by hussein alhamed .
+      © Designed & developed by hussein alhamed .
     </div>
   );
 };
